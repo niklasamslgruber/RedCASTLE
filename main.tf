c489339ef133ce0bf6720c6e068dfed4cc80da43
Julian Legler <julian.legler@campus.tu-berlin.de>
2021-08-09 11:52:28 +0200

    feat: added support for RabbitMQ deployment

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "google_compute_instance" "node_red_cloud" {
 }
 
 resource "google_compute_instance" "mqtt_broker_cloud" {
-  name         = "mqtt-borker-cloud-server"
+  name         = "mqtt-broker-cloud-server"
   machine_type = "n2-standard-2"
 
   boot_disk {
@@ -107,24 +107,89 @@ resource "google_compute_instance" "mqtt_broker_cloud" {
   }
   
   provisioner "file" {
-    source      = "configs/mosquitto.conf"
-    destination = "~/mosquitto.conf"
+    source      = "scripts/installRabbitMQ.sh"
+    destination = "~/installRabbitMQ.sh"
   }
 
   provisioner "remote-exec" { 
 #    interpreter = ["/bin/bash" ,"-c"]
     inline = [
-      #"sudo apt update && sudo apt upgrade -y",
-      #"sudo apt install git python3-pip python3-zmq apt-transport-https ca-certificates gnupg -y",
-      #"pip3 install --upgrade pip",
-      "sudo mkdir -p /etc/mosquitto/conf.d",
-      "sudo mv ~/mosquitto.conf /etc/mosquitto/conf.d/mosquitto.conf",
-      "sudo apt update",
-      "sudo apt install mosquitto mosquitto-clients -y",
+      "sudo apt install libncurses5 -y",
+      "chmod +x installRabbitMQ.sh",
+      "sudo ./installRabbitMQ.sh",
+      "sudo rabbitmq-plugins enable rabbitmq_mqtt",
+      "sudo rabbitmq-plugins enable rabbitmq_management",
+      "sudo service rabbitmq-server restart",
+      "sudo rabbitmqctl add_user peng peng",
+      "sudo rabbitmqctl set_user_tags peng administrator",
+      "echo 'Management UI should now be available via http://${self.network_interface.0.access_config.0.nat_ip}:15672'"
    ]
   }  
 }
 
+
+resource "google_compute_instance" "emulator_cloud" {
+  name         = "emulator-cloud-server"
+  machine_type = "n2-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+  metadata = {
+    ssh-keys = "peng:${file("~/.ssh/id_rsa.pub")}"
+  }
+  
+  connection {
+    type  = "ssh"
+    host  = self.network_interface.0.access_config.0.nat_ip
+    user  = "peng"
+    port  = 22
+#    agent = true
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+
+
+  provisioner "remote-exec" { 
+#    interpreter = ["/bin/bash" ,"-c"]
+    inline = [
+      "sudo apt update && sudo apt upgrade -y",
+      "sudo apt install git nodejs npm python3-pip screen -y",
+      "npm install npm@latest -g",
+      "pip3 install --upgrade pip",
+      "npm install -g --unsafe-perm node-red",
+      "git clone https://github.com/niklasamslgruber/RedCASTLE.git",
+      "cd RedCASTLE",
+      "git checkout dev_advanced_benchmark", # change branche accordingly to needs
+      "chmod +x setup.sh",
+      "pip3 install -r requirements.txt",
+      # install RedCastle npm dependencies
+      "npm install package.json",
+      # TODO: why is 'npm install package.json' not enough?
+      "cd ~/.node-red/",
+      "npm install node-red-dashboard",
+      #"sudo chown -R peng ./*",
+      "npm install node-red-dashboard",
+      "cd ~/RedCASTLE/",
+      # inject broker ip in the redcastle config file 
+      # TODO: only work if the config is not changed to something else then localhost
+      "sed -i 's/localhost/${google_compute_instance.mqtt_broker_cloud.network_interface.0.access_config.0.nat_ip}/g' CASTLE/src/config.json",
+      # inject broker ip in the flow file directly changing the mqtt node config
+      # TODO: only work if the config is not changed to something else then localhost
+      "sed -i 's/localhost/${google_compute_instance.mqtt_broker_cloud.network_interface.0.access_config.0.nat_ip}/g' flow.json",
+      # TODO: make it run in the background. Had errors with screen and '&' and i dont know why its not working as expected
+      "node-red flow.json"
+   ]
+  }  
+}
 
 resource "google_compute_network" "vpc_network" {
   name                    = "cloud-network"
@@ -146,7 +211,7 @@ resource "google_compute_firewall" "allow_ssh" {
   source_ranges = ["0.0.0.0/0"]
 }
 resource "google_compute_firewall" "allow_internet" {
-  name = "cloud-network-internal-allow-internet-${random_id.id.hex}"
+  name = "cloud-network-internal-allow-http-${random_id.id.hex}"
   network = google_compute_network.vpc_network.self_link
 
   allow {
@@ -177,3 +242,68 @@ resource "google_compute_firewall" "allow_mqtt" {
   }
   source_ranges = ["0.0.0.0/0"]
 }
+
+resource "google_compute_firewall" "allow_rabbitmq" {
+  name = "cloud-network-internal-allow-rabbitmq-${random_id.id.hex}"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "tcp"
+    ports = ["15672", "5672", "5671", "4369", "25672"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+
+
+# Backup MOSQUITTO
+
+
+#resource "google_compute_instance" "mqtt_broker_cloud" {
+#  name         = "mqtt-broker-cloud-server"
+#  machine_type = "n2-standard-2"
+#
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-10"
+#    }
+#  }
+#
+#  network_interface {
+#    # A default network is created for all GCP projects
+#    network = google_compute_network.vpc_network.self_link
+#    access_config {
+#    }
+#  }
+#  metadata = {
+#    ssh-keys = "peng:${file("~/.ssh/id_rsa.pub")}"
+#  }
+#  
+#  connection {
+#    type  = "ssh"
+#    host  = self.network_interface.0.access_config.0.nat_ip
+#    user  = "peng"
+#    port  = 22
+#    agent = true
+#    private_key = "${file("~/.ssh/id_rsa")}"
+#  }
+  
+#  provisioner "file" {
+#    source      = "configs/mosquitto.conf"
+#    destination = "~/mosquitto.conf"
+#  }
+#
+#  provisioner "remote-exec" { 
+#    interpreter = ["/bin/bash" ,"-c"]
+#    inline = [
+#      #"sudo apt update && sudo apt upgrade -y",
+#      #"sudo apt install git python3-pip python3-zmq apt-transport-https ca-certificates gnupg -y",
+#      #"pip3 install --upgrade pip",
+#      "sudo mkdir -p /etc/mosquitto/conf.d",
+#      "sudo mv ~/mosquitto.conf /etc/mosquitto/conf.d/mosquitto.conf",
+#      "sudo apt update",
+#      "sudo apt install mosquitto mosquitto-clients -y",
+#   ]
+#  }  
+#}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "cloud-server-public-ip" {
 output "mqtt-broker-public-ip" {
   value = google_compute_instance.mqtt_broker_cloud.network_interface.0.access_config.0.nat_ip
 }
+
+output "emulator-public-ip" {
+  value = google_compute_instance.emulator_cloud.network_interface.0.access_config.0.nat_ip
+}
+
